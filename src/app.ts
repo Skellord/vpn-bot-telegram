70514ed6dad0e3aee3997fe0cb1607840741f360
Skellord <skell0rd@mail.ru>
2023-08-29 23:31:43 +0500

    fix: add pm2 start

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { Telegraf } from 'telegraf';
 import { message } from 'telegraf/filters';
 import 'dotenv/config'
+import * as express from 'express';
 
 import {
   connectionButtons,
@@ -30,9 +31,11 @@ const port = process.env.PORT ? Number(process.env.PORT) : 5001;
 const domain = process.env.WEBHOOK_URL ?? '';
 const launchConfig = process.env.NODE_ENV === 'production' ? { webhook: { domain, port } } : undefined;
 
-if (process.env.NODE_ENV === 'production' && process.env.WEBHOOK_URL) {
-  bot.telegram.setWebhook(process.env.WEBHOOK_URL);
-}
+// if (process.env.NODE_ENV === 'production' && process.env.WEBHOOK_URL) {
+//   bot.telegram.setWebhook(process.env.WEBHOOK_URL);
+// }
+
+const app = express();
 
 bot.start(startHandler);
 
@@ -51,4 +54,16 @@ bot.on('pre_checkout_query', preCheckoutHandler);
 
 bot.on(message('successful_payment'), successfulPaymentHandler)
 
-bot.launch(launchConfig);
+bot.launch();
+
+// bot.telegram.setWebhook('https://1547223-cc62931.twc1.net/bot');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.use(bot.webhookCallback('/bot'));
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+})
